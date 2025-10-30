10b883ba9db66a2ced8b96ec9ae7cee002b1363d
reitaghchi <ren.len2426@gmail.com>
2025-10-30 22:09:36 +0900

    [add]テキストデータを作成

---
+++ Assets/Code/Scripts/TalkSystem/Entity/Text/TextData.cs
@@ -0,0 +1,17 @@
+namespace ScriptedTalkSystem.TalkSystem.Entity.Text
+{
+    /// <summary>
+    /// 会話文のデータ
+    /// </summary>
+    public class TextData 
+    {
+        public string Text { get; private set; }
+        public int CharacterID { get; private set; }
+
+        public TextData(string text, int characterID)
+        {
+            Text = text;
+            CharacterID = characterID;
+        }
+    }
+}

72d306361f88a536e387d8ecfff40a0593ca2f80
reitaghchi <ren.len2426@gmail.com>
2025-10-31 13:04:19 +0900

    [update]インターフェースの中にグループと行数を保持するプロパティ追加

--- Assets/Code/Scripts/TalkSystem/UseCase/TextBox/IContextCache.cs
+++ Assets/Code/Scripts/TalkSystem/UseCase/TextBox/IContextCache.cs
@@ -2,8 +2,13 @@ using ScriptedTalk.TalkSystem.Entity.TalkData;
 
 namespace ScriptedTalk.TalkSystem.UseCase.TextBox
 {
+    /// <summary>
+    /// 会話状況の保持をするためのインターフェース
+    /// </summary>
     public interface IContextCache
     {
+        public int GroupNumber { get; }
+        public int LineNumber { get; }
         public Context GetContext();
         public void SetContext(Context context);
     }

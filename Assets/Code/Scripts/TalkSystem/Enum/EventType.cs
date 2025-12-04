5d8beb8092d55228dcd5257909a317f38d35c522
reitaghchi <ren.len2426@gmail.com>
2025-12-04 17:06:16 +0900

    [add]EventTypeEnumを改めて作成

---
+++ Assets/Code/Scripts/TalkSystem/Enum/EventType.cs
@@ -0,0 +1,11 @@
+public enum EventType
+{
+    ShowCharacter,
+    HideCharacter,
+    HighlightCharacter,
+    AnimateCharacter,
+    AnimateBackground,
+    MoveCharacter,
+    PlaySound,
+    ChangeBackground
+}

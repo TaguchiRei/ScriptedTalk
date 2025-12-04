f116e1ef4ed6cf821458951ae4c37605d25a8b57
reitaghchi <ren.len2426@gmail.com>
2025-12-04 23:22:05 +0900

    [add]イベントリポジトリのメソッド作成

--- Assets/Code/Scripts/TalkSystem/UseCase/Event/IEventRepository.cs
+++ Assets/Code/Scripts/TalkSystem/UseCase/Event/IEventRepository.cs
@@ -1,4 +1,15 @@
-public  interface IEventRepository
+using System.Numerics;
+using ScriptedTalk.Code.Scripts.TalkSystem.Entity.Event;
+using ScriptedTalk.TalkSystem.Entity.Character;
+
+namespace ScriptedTalk.TalkSystem.UseCase.Event
 {
-    
+    public interface IEventRepository
+    {
+        public Vector3 GetCharacterPosition(EventData eventData);
+
+        public string GetAnimationClip(EventData eventData);
+
+        public CharacterData GetCharacter(EventData eventData);
+    }
 }

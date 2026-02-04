97e2e8f2302089aec06ddda1d80fad0a90bdf4ef
reitaghchi <ren.len2426@gmail.com>
2026-02-04 22:12:22 +0900

    [add]会話中の一時データを保持する用のクラス作成
    会話中のみ生存し、それ以外の時は存在しないクラスとして設計

---
+++ Assets/Code/Scripts/TalkSystem/UseCase/TextBox/TalkRuntimeModel.cs
@@ -0,0 +1,102 @@
+using UnityEngine;
+
+namespace ScriptedTalk
+{
+    public class TalkRuntimeModel
+    {
+        private ContextData _contextData;
+        private string _readingGroup;
+        private int _readingGroupIndex;
+        private int _readingLine;
+
+        private bool _lastLine;
+        private string[] _selectionGuids;
+
+        public TalkRuntimeModel(ContextData contextData)
+        {
+            _contextData = contextData;
+            _readingGroup = _contextData.StartGroupGuid;
+            _readingLine = -1;
+            _lastLine = false;
+            _readingGroupIndex = GetIndex();
+        }
+
+        /// <summary>
+        /// 会話の途中から再開するときに読み込む
+        /// </summary>
+        /// <param name="contextData"></param>
+        /// <param name="readingGroup"></param>
+        /// <param name="readingLine"></param>
+        public TalkRuntimeModel(ContextData contextData, string readingGroup, int readingLine)
+        {
+            _contextData = contextData;
+            _readingGroup = readingGroup;
+            _readingLine = readingLine;
+            _lastLine = false;
+
+            _readingGroupIndex = GetIndex();
+        }
+
+        /// <summary>
+        /// 次の行を取得する
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
+        public bool TryGetNextLine(out TalkLineData line)
+        {
+            //最終行に到達あるいは到達済みの時に読んだ時falseを返し、最終行の内容を返す
+            if (_contextData.TryGetLine(_readingGroupIndex, _readingLine, out line) && !_lastLine)
+            {
+                _readingLine++;
+                return true;
+            }
+            else
+            {
+                _lastLine = true;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 選択肢を取得する
+        /// </summary>
+        /// <param name="selections"></param>
+        /// <returns>選択肢がない場合はfalse</returns>
+        public bool GetSelection(out string[] selections)
+        {
+            if (_contextData.TryGetQuestion(_readingGroupIndex, out var questions))
+            {
+                selections = new string[questions.Count];
+                _selectionGuids = new string[questions.Count];
+                for (int i = 0; i < questions.Count; i++)
+                {
+                    selections[i] = questions[i].SelectionTitle;
+                    _selectionGuids[i] = questions[i].NextGroupGuid;
+                }
+
+                return true;
+            }
+            else
+            {
+                selections = null;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 選択肢によって次のContextに移動する。
+        /// GetSelectionメソッドで取得した質問配列のインデックス番号を指定する
+        /// </summary>
+        /// <param name="selectionIndex"></param>
+        public void SelectNextGroup(int selectionIndex)
+        {
+            _readingGroup = _selectionGuids[selectionIndex];
+            _readingGroupIndex = GetIndex();
+        }
+
+        private int GetIndex()
+        {
+            return _contextData.GetGroupIndex(_readingGroup);
+        }
+    }
+}

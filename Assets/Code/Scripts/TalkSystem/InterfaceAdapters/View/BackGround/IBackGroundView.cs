377bbc08f4d6d9af3a7bea7ab0e5a9e9ba0bebb1
reitaghchi <ren.len2426@gmail.com>
2026-02-05 01:56:00 +0900

    [remove]フォルダ整理

--- Assets/Code/Scripts/TalkSystem/InterfaceAdapters/View/BackGround/IBackGroundView.cs
+++
@@ -1,8 +0,0 @@
-public interface IBackGroundView
-{
-    void SetBackground(string backgroundKey);
-
-    void AnimateBackground(string animationKey);
-
-    void PlayEffect(string effectKey);
-}
--- Assets/Code/Scripts/TalkSystem/View/Event/IEvent.cs
+++ Assets/Code/Scripts/TalkSystem/View/Event/IEvent.cs
@@ -1,8 +1,11 @@
+using System;
+
 /// <summary>
 /// イベントの実行の実装を書くクラスが実装するインターフェース
 /// </summary>
 public interface IEvent
 {
+    public Action EndAction { get; set; }
     public void Execute();
 
     public void Skip();
